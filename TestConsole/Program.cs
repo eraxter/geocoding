77d7e9e1be1744cf313dec161a41df4e299ba013
Erik Raxter <erikrax@gmail.com>
2026-03-09 06:47:59 -0400

    added console app TestConsole

---
+++ TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geocoding.Providers;
+
+namespace TestConsole;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\n\npress any key to continue...");
+        Console.ReadKey();
+    }
+}
